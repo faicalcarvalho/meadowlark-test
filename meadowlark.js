b6cb58ebc595f84a3fee79c9744dd6760af50841
Faiçal Farhat de Carvalho <faicalcarvalho@gmail.com>
2015-05-18 19:37:05 -0300

    Meadowlark dev going on

--- meadowlark.js
+++ meadowlark.js
@@ -1,3 +1,31 @@
+function getWeatherData() {
+	return {
+		locations: [
+			{
+				name: "Portland",
+				forecastUrl: "http://www.wunderground.com/US/OR/Portland.html",
+				iconUrl: "http://icons-ak.wxug.com/i/c/k/cloudy.gif",
+				weather: "Overcast",
+				temp: "54.1 F (12.3 C)"
+			},
+			{
+				name: "Bend",
+				forecastUrl: "http://www.wunderground.com/US/OR/Bend.html",
+				iconUrl: "http://icons-ak.wxug.com/i/c/k/partlycloudy.gif",
+				weather: "Partly Cloudy",
+				temp: "55.0 F (12.8 C)"
+			},
+			{
+				name: "Manzanita",
+				forecastUrl: "http://www.wunderground.com/US/OR/Manzanita.html",
+				iconUrl: "http://icons-ak.wxug.com/i/c/k/rain.gif",
+				weather: "Light Rain",
+				temp: "55.0 F (12.8 C)"
+			}
+		]
+	};
+}
+
 var express = require("express");
 var app = express();
 var fortune = require("./lib/fortune.js");
@@ -14,12 +42,29 @@ app.set("port", process.env.PORT || 3000);
 // public
 app.use(express.static(__dirname + "/public"));
 
+// special url for tests
+app.use(function(req, res, next) {
+	res.locals.showTests = app.get("env") !== "production" &&
+			req.query.test === "1";
+	next();
+});
+
+// weather partial via res.locals
+app.use(function(req, res, next) {
+	if (!res.locals.partials) res.locals.partials = {};
+	res.locals.partials.weather = getWeatherData();
+	next();
+});
+
 // routes
 app.get("/", function(req, res) {
 	res.render("home");
 });
 app.get("/about", function(req, res) {
-	res.render("about", {fortune: fortune.getFortune()});
+	res.render("about", {
+		fortune: fortune.getFortune(),
+		pageTestScript: "/qa/tests-about.js"
+	});
 });
 
 // custom 404 page
